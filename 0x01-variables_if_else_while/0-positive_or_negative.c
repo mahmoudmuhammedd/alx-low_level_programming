c7074a54bf28e8ab1351a734dd1d4aaee1802965
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-14 16:04:23 -0700

    pos and neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Description: program to show whether positive or negative
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,15 +14,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	{
+	if (n > 0)
 	printf("%i is positive\n", n);
+	else if (n == 0)
+	{
+	printf("%i is zero\n", n);
 	}
-	else if ( n == 0 ) {
-        printf( "%i is zero\n", n);
-    }
-    else {
-        printf( "%i is negative\n", n);
-    }
+	else
+	{
+	printf("%i is negative\n", n);
+	}
 	return (0);
 }
