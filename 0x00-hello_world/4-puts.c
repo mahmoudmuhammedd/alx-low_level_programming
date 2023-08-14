f063fae5f74b13a71cf6625295cb0e4ac1199618
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-14 11:12:30 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Description: A C program using puts function
+ * main : Entry Point of program
+ * return: Always return 0 (Success)
+ * puts: Print newline without escape character
+ * Descripion: A C program using puts function not printf
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
