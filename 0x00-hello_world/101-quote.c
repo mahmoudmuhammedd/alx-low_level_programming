e5316bfe9b9b95aeb9caaa24e786e6b1ca4e4094
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-14 12:24:43 -0700

    printing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
