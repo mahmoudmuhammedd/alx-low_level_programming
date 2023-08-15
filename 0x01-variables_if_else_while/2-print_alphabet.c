6b050ad1b811fc9cf125ec2089f20f4480c6f340
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-15 09:26:50 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Alphabet printer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+for (i = 97; i < 123; i++)
+{
+	putchar(i);
+}
