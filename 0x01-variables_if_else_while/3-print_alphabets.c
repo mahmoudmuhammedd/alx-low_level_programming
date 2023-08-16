148ee5d2e3e33ddfa51e97364a0ba29ca2e5d7a8
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-15 23:42:22 -0700

    lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Alphabet in lower and upper case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	char CH = 'A'
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
+}
