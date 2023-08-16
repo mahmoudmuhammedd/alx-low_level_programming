ad887834549a1c6cb5733cdfc545e122becb1ef7
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-16 14:50:15 -0700

    special char

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,15 @@ int main(void)
 	int num = 0;
 
 	while (num <= 9)
-{
-	putchar(num + '0');
-	putchar(",");
-	putchar(' ');
-	num++;
-}
+	{
+		putchar(num + 48);
+		if (num != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		num++;
+	}
 	putchar('\n');
 	return (0);
 }
