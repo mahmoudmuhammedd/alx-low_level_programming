a8fa8f75d8a51d5380e4c736d24c4e124d73c718
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-16 10:02:00 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+	char ch = 'A';
+
+	if (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	else if (ch <= F)
+	{
+	putchar(ch);
+	ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
