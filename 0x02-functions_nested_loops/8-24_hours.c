3ae4967f095ce6c97ed106733ee10ffd0fffb0b8
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-22 15:01:03 -0700

    {minutes of day}

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,14 @@ void jack_bauer(void)
 
 	for (hr = 0; hr <= 23; hr++)
 	{
+		for (min = 0; min <= 59; min++)
+		{
 		_putchar((hr / 10) + '0');
 		_putchar((hr % 10) + '0');
 		_putchar(':');
 		_putchar((min / 10) + '0');
 		_putchar((min % 10) + '0');
 		_putchar('\n');
+		}
 	}
 }
