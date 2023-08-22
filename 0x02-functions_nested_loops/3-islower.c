0fb8fea8c5dc5887b40ba7052b176fd9b9a795b9
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-22 06:17:41 -0700

    {check lowercase}

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * islower - detects lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	char ch[27];
+	int c;
+
+	for (c = 0; c < 26; c++)
+	{
+	char[ch] = 'a' + c;
+	}
+
+	arr[ch] = '\0';
+	islower();
+}
