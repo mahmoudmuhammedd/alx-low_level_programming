e3a6d53bfa3ecb13c4c8293e23bd7dcc0ec54c08
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-22 16:18:33 -0700

    {9 times}

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,14 +6,24 @@
 
 void times_table(void)
 {
-	int i, j, times_table;
+	int i, j, prod;
 
 	for (i = 0; i <= 9; i++)
 	{
+	_putchar(48);
 	for (j = 0; j <= 9; j++)
 	{
-		times_table = i * j;
-		printf("%d, ", times_table);
+		_putchar(',');
+		_putchar(' ');
+
+		prod = i * j;
+		if (prod <= 9)
+			_putchar(' ');
+		else
+		{
+			_putchar(prod / 10 + 48);
+			_putchar(prod % 10 + 48);
+		}
 	}
 		_putchar('\n');
 	}
