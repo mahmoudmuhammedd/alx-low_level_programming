0e61e13d7a4d342735f19ef718929a90f6b3bf26
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-23 09:25:45 -0700

    {print most num}

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,7 +6,7 @@
  * Return: numbers from 0 to 9 except 2 and 4
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 
 {
 	int i;
@@ -15,7 +15,7 @@ void print_most_numbers(void);
 	{
 		if (i == 2 || i == 4)
 		{
-			i++;
+			continue;
 		}
 		else
 		{
