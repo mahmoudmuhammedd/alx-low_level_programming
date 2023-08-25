060db6ff7ac7e212510e07b654ac357b70e5771e
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-25 13:27:38 -0700

    {in reverse}

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int i, l;
+	char temp;
+
+	for (i = 0; s != '\n'; i++)
+		;
+	for (l = 0; l < (i / 2); l++)
+		temp = s[l];
+		s[l] = s[i - 1 -l];
+		s[i - 1 -l] = temp;
+}
