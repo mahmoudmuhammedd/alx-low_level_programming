58d04f80464b1f2dcabc42a5a653e80e0ba38115
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-25 09:10:06 -0700

    {string length}

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - shows the length of a string 
+ *
+ * @s: input of a string
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	size_t strlen(const char *s);
+	(void) s;
+	return (0);
+}
