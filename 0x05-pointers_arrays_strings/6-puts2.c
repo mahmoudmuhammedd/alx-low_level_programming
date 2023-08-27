e8309c3b7d237d087910ea640ba8d41c0be3de95
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-27 12:46:13 -0700

    {print other string}

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0';)
+	for (i = 0; str[i] != '\0';i++)
 	{
-		if (str[(i % 2) == 0])
+		if (i % 2 == 0)
 
 			_putchar(str[i]);
-		else
-			i++;
 	}
 	_putchar('\n');
 }
