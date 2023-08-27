60b118eca2f51533600c9f926eb68bbcf7a13231
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-27 12:18:12 -0700

    {print other string}

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - print other string
+ *
+ * @str: input parameter for string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (str[i % 2 = 0])
+
+			_putchar(s[i]);
+		else
+			i++;
+	}
+	_putchar('\n');
+}
