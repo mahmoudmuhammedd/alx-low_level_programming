0226be6cb10b4042ac308ca033b8696581c16c9c
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-30 17:17:27 -0700

    {print string in reverse}

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,10 +10,10 @@
 void _print_rev_recursion(char *s)
 {
 		if (*s == '\0')
-			_putchar('\n');
+			return;
 		else
 			{
-			_print_rev_recursion(s + 1);	
+			_print_rev_recursion(s + 1);
 			_putchar(*s);
 			}
 }
