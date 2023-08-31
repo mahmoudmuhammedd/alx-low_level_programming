8ab769520221a57639dbc93ee6e6d06b5bb7b07b
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-30 17:38:39 -0700

    {string length}

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,7 +13,7 @@ int _strlen_recursion(char *s)
 {
 	int n = 0;
 
-	if (*s == '\0')
-		return (n);
-	n += _strlen_recursion(s + 1) + 1;
+	if (*s > '\0')
+		n += _strlen_recursion(s + 1) + 1;
+	return (n);
 }
