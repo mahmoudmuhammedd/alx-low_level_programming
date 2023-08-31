2dda31cdb26709606368bd244c3ca08a2e99be49
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-30 17:07:41 -0700

    {print a string followed by a new line}

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include "main.h"
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
