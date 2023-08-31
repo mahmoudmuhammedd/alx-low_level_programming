5cb4381913f2216bcb16c6a0eb3aa75e3e0ba5f0
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-30 17:44:30 -0700

    {factorial of a number}

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n > 0)
+	{
+		factorial(n * (n - 1));
+		n--;
+	}
+	else 
+	{
+		return (-1);
+	}
+}
