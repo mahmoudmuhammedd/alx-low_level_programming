fecc6589e75b5fa97f4969a09c02344a85de4540
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-08-30 17:14:30 -0700

    {print string in reverse}

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: input parameter for string
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+		if (*s == '\0')
+			_putchar('\n');
+		else
+			{
+			_puts_recursion(s + 1);	
+			_putchar(*s);
+			}
+}
