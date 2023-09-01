cfcf8ba8e164d99b8f7b57dbf8d4dda221ed072c
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-09-01 15:56:47 -0700

    {static library}

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copy string pointed by src
+ *
+ * @dest: char type string
+ *
+ * @src: char type string
+ * Description: copy string pointed by pointer src to dest
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
