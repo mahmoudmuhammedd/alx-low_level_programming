5a7ed23d16a6ccafd2e1c8f467dd8ece06eb2add
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-09-03 15:17:15 -0700

    argument number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print program name followerd by anew line
+ * @argc: int
+ * @argv: list
+ * Return: Always 0.
+ */
+int main(int argc, const char *argv[])
+{
+        (void)argc;
+        printf("%d\n", argc);
+        return (0);
+}
