b68846e45b6b1a0db6e012b86fa2136bb6896f05
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-09-03 16:16:13 -0700

    print prod

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print prod
+ * @argc: int
+ * @argv: list
+ * Return: 0 if ok 1 if error
+ */
+int main(int argc, char **argv)
+{
+	int prod;
+
+	if (argc == 3)
+	{
+		prod = argv[1] * argv[2];
+		printf("%d\n", prod);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
