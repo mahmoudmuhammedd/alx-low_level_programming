d439c67e8a16b4d229fbd53fbc0d9a97656235e9
mahmoudmuhammedd <mahmmmoudmuhammmed@gmail.com>
2023-09-03 16:49:06 -0700

    print add of postive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print prod
+ * @argc: int
+ * @argv: list
+ * Return: 0 if ok 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int add = 0;
+	char *s;
+
+	while (--argc)
+	{
+		for (s = argv[argc]; *s; s++)
+			if (*s < '0' || *s > '9')
+				return (printf("Error\n"), 1);
+		add += atoi(argv[argc]);
+	}
+	printf("%d\n", add)
+	return (0);
+}
