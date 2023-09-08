f55002dfee5d9a7378d03c951350e978dc0313dc
root <root@DESKTOP-VDNTBUQ>
2023-09-08 13:27:24 +0200

    {free grid}

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-alloc_grid.c"
+/**
+ * free_grid - free grid
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	free(grid[height]);
+}
+int freefn(*grid)
+{
+	int **alloc_grid(int width, int height);
+	return (grid);
+}
