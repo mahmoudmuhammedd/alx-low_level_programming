af12764307d6745ad7e8531e5a1f7f17dbeeb67b
root <root@DESKTOP-VDNTBUQ>
2023-09-08 14:37:18 +0200

    {free grid}

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,16 +5,14 @@
 /**
  * free_grid - free grid
  * @grid: the address of the two dimensional grid
- * @width: width of the grid
  * @height: height of the grid
  * Return: Nothing.
  */
 void free_grid(int **grid, int height)
 {
-	free(grid[height]);
-}
-int freefn(*grid)
-{
-	int **alloc_grid(int width, int height);
-	return (grid);
+	int i;
+
+	for (i = 0; i <= height; i++)
+		free(grid[i]);
+	free(grid);
 }
