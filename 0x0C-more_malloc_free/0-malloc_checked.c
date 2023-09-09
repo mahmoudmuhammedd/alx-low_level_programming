7ceaeffaafc48cb3887bb92018968ec36ba3363f
root <root@DESKTOP-VDNTBUQ>
2023-09-09 22:59:03 +0200

    {a function that allocates memory using malloc}

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - function that locates memory
+ * @b: input parameter
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *s;
+
+	s = malloc(sizeof(int) * b);
+	if (s == NULL)
+	{
+		exit(98);
+	}
+}
