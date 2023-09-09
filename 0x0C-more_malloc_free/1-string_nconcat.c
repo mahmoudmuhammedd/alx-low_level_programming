8f1d20f95653ff82dbd4357e6cd2b74ba5b4aada
root <root@DESKTOP-VDNTBUQ>
2023-09-10 00:47:32 +0200

    {concatenates two strings}

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,8 +31,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i] = s1[i];
 	for (j = 0; j < n; j++)
 		{
-			s[i] = s2[j];
-			i++;
+		s[i] = s2[j];
+		i++;
 		}
 	s[i] = '\0';
 	return (s);
