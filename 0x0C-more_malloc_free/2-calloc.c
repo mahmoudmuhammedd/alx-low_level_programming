44682c412da44d65a21d3f0aa37b12c5f9f45d24
root <root@DESKTOP-VDNTBUQ>
2023-09-10 22:35:53 +0200

    {similar to calloc}

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of mem bytes
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *m;
+	int i = 0, l = 0;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	m = malloc(size * nmemb);
+	l = size * nmemb;
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	if (i < l)
+	{
+		m[i] = 0;
+		i++;
+	}
+	return (m);
+}
