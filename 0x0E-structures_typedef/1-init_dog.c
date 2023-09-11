8e661587efaf8fe8d6ca964d3463548a86c478f7
root <root@DESKTOP-VDNTBUQ>
2023-09-11 23:17:41 +0200

    {initializing the struct}

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - fn for dog
+ * struct dog - dog
+ * @d: struct var
+ * @name: input for name
+ * @age: input for age
+ * @owner: input for owner
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog d = {{'\0'}, 0, {'\0'}};
+}
