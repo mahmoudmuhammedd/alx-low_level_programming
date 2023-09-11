9f88bbc1533a64a0033c2311317a23b7233e5097
root <root@DESKTOP-VDNTBUQ>
2023-09-12 00:36:23 +0200

    {print fn}

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,18 +7,14 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
-		printf("Name: (nil)");
-	if (d->owner == NULL)
-		printf("Owner: (nil)");
 	if (d == NULL)
-	{
-	return;
-	}
-	else
-	{
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
-	}
 }
