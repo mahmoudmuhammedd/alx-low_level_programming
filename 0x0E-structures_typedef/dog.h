29098911bb762278d0491d14c279be349190fb7a
root <root@DESKTOP-VDNTBUQ>
2023-09-11 22:55:12 +0200

    {poppy}

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,14 @@
 #define DOG_H
 
 /**
- * dog - poppy
+ * struct dog - poppy
  * @name: input for name
  * @age: input for age
  * @owner: input for owner
  * Return: Always 0.
  */
-struct dog{
+struct dog
+{
 char *name;
 float age;
 char *owner;
