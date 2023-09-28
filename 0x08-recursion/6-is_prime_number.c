fd43719eb2b37d733fe84a2f32f6cd846ab96ec8
root <root@DESKTOP-VDNTBUQ>
2023-09-29 01:00:56 +0200

    {prime number}

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - function to print prime numbers
+ * @n: input parameter
+ * @check: input parameter
+ * Return: prime number
+ */
+int prime(int n, int check);
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
+/**
+ * prime - function to return prime
+ * @n: input parameter
+ *
+ * @check: input parameter
+ * Return: 1 if prime 0 if not
+ */
+int prime(int n, int check)
+{
+	if (check >= n && n > 1)
+		return (1);
+	else if (n % check == 0 || n <= 1)
+		return (0);
+	else
+		return (prime(n, check + 1));
+}
