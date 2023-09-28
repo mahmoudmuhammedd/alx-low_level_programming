6b2c6ee3d85ded574170fcff3072e68a3ea6ba1f
root <root@DESKTOP-VDNTBUQ>
2023-09-29 00:27:19 +0200

    {length of a string}

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - string length
+ *
+ * @s: pointer for string
+ *
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+    int len = 0;
+
+	if (*s > '\0')
+		len += _strlen_recursion(s + 1) + 1;
+	return (len);
+
+}
