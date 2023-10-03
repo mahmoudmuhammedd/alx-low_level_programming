b68490b086c9e054734b634103e120ad76d4b364
root <root@DESKTOP-VDNTBUQ>
2023-10-03 03:12:41 +0200

    {arguments}

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  * main - print program name followerd by anew line
+ * @argc: int
+ * @argv: list
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	exit(EXIT_SUCCESS);
+}
