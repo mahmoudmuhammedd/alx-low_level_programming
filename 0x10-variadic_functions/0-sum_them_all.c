418f53692d77aa02b028e2483117d074bb6c5475
root <root@DESKTOP-VDNTBUQ>
2023-10-15 16:05:09 +0200

    {sum of int}

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,19 +1,24 @@
 #include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - get the sum
+ * @n: number of arg
+ * @...: the integers we sum
+ *
+ * Return: integer sum.
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	int i, sum;
+	int i, sum = 0;
+
 	va_start(args, n);
-	sum = 0;
 	if (n == 0)
 		return (0);
-	else
+	for (i = 0; i < n; i++)
 	{
-	for(i = 0; i < n; i++)
-	{
-		sum+= va_arg(args, int);
+		sum += va_arg(args, int);
 		va_end(args);
 		return (sum);
-	}
 	}
 }
